2e1442a548a189cfa942bd66ec7ec44ba332694a
Alain HUA <ahua@e2r7p16.42.fr>
2017-05-23 23:19:26 +0200

    truc

--- app-server/models/comment.js
+++ app-server/models/comment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
-const commentSchema = mongoose.Schema({
+const commentSchema = Schema({
     user_id: {type: Schema.Types.ObjectId, ref: 'User' },
     imdb_code: { type: String },
     comment: {type: String},
