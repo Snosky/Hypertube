1ce347c0de4012484799255eae4462858d7ccab5
Snosky <tpayen@student.42.fr>
2017-05-25 00:43:59 +0200

    Loading on oauth

--- app-client/app/social-auth/social-auth.component.js
+++ app-client/app/social-auth/social-auth.component.js
@@ -20,28 +20,35 @@ var SocialAuthComponent = (function () {
         this.router = router;
         this.route = route;
         this.flash = flash;
+        this.loading = false;
     }
     SocialAuthComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
         var provider = this.route.snapshot.params['provider'];
-        if (provider)
+        if (provider) {
+            this.loading = true;
             this.socialAuthService.login(provider, this.route.snapshot.queryParams)
                 .then(function (success) {
                 if (success === true) {
+                    _this.loading = false;
                     _this.flash.success('You are now logged in.', true);
                     _this.router.navigate([_this.returnUrl]);
                 }
             })
                 .catch(function (error) {
+                _this.loading = false;
                 _this.flash.error('An error occurred. Please retry.');
                 console.log('error', error);
             });
+        }
     };
     SocialAuthComponent.prototype.facebookLogin = function () {
+        this.loading = true;
         window.location.href = 'https://www.facebook.com/v2.9/dialog/oauth?client_id=1006427196157157&scope=email&redirect_uri=http://localhost:3001/auth/facebook';
     };
     SocialAuthComponent.prototype.login42 = function () {
+        this.loading = true;
         window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=b891f269ed7d8f5bca65afaccb26aeffe83e885d8fe173a2c4fd51668daee2fb&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2F42&response_type=code';
     };
     return SocialAuthComponent;
--- app-client/app/social-auth/social-auth.component.ts
+++ app-client/app/social-auth/social-auth.component.ts
@@ -12,6 +12,7 @@ import {FlashService} from "../flash.service";
 })
 export class SocialAuthComponent implements OnInit {
     returnUrl: string;
+    loading = false;
 
     constructor(
         private socialAuthService: SocialAuthService,
@@ -24,25 +25,31 @@ export class SocialAuthComponent implements OnInit {
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
 
         let provider = this.route.snapshot.params['provider'];
-        if (provider)
+        if (provider) {
+            this.loading = true;
             this.socialAuthService.login(provider, this.route.snapshot.queryParams)
                 .then((success) => {
                     if (success === true) {
+                        this.loading = false;
                         this.flash.success('You are now logged in.', true);
                         this.router.navigate([this.returnUrl]);
                     }
                 })
                 .catch((error) => {
+                    this.loading = false;
                     this.flash.error('An error occurred. Please retry.');
                     console.log('error', error);
                 });
+        }
     }
 
     facebookLogin() {
+        this.loading = true;
         window.location.href = 'https://www.facebook.com/v2.9/dialog/oauth?client_id=1006427196157157&scope=email&redirect_uri=http://localhost:3001/auth/facebook';
     }
 
     login42() {
+        this.loading = true;
         window.location.href= 'https://api.intra.42.fr/oauth/authorize?client_id=b891f269ed7d8f5bca65afaccb26aeffe83e885d8fe173a2c4fd51668daee2fb&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2F42&response_type=code'
     }
 
